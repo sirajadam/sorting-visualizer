7b7efe174a124a69943a127341dc70e9a8516b3b
adam <adamsirajdeveloper@gmail.com>
2021-09-10 12:03:05 +0200

    added slider for changing array size dynamically with useEffect

--- src/components/sorting/SortingControls.js
+++ src/components/sorting/SortingControls.js
@@ -1,8 +1,14 @@
+import { useState } from "react/cjs/react.development";
 import styles from "./SortingControls.module.css";
 
 const SortingControls = (props) => {
   const callerHandler = () => {
     props.onInsertionSort();
+  };
+
+  const onChangeHandler = (e) => {
+    props.onSetSize(e.target.value);
+    console.log(props.size);
   };
 
   return (
@@ -11,8 +17,31 @@ const SortingControls = (props) => {
         <button onClick={props.onReset} className={styles.btn}>
           Generate
         </button>
+        <div className={styles["size-ctrl"]}>
+          <label>Size</label>
+          <input
+            type="range"
+            min="5"
+            max="150"
+            value={props.size}
+            onChange={onChangeHandler}
+            className={styles.range}
+          />
+        </div>
         <button className={styles.btn} onClick={callerHandler}>
           Insertion Sort
+        </button>
+        <button className={styles.btn} onClick={callerHandler}>
+          Selection Sort
+        </button>
+        <button className={styles.btn} onClick={callerHandler}>
+          Bubble Sort
+        </button>
+        <button className={styles.btn} onClick={callerHandler}>
+          Merge Sort
+        </button>
+        <button className={styles.btn} onClick={callerHandler}>
+          Quick Sort
         </button>
       </div>
     </section>
